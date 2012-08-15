cce31940ff3691020de5109fac771af5d6b7d118
Scott Torborg <scott@cartlogic.com>
2012-08-15 00:21:12 -0700

    make randompass.py a little more friendly for typing into a console

--- scripts/randompass.py
+++ scripts/randompass.py
@@ -7,6 +7,7 @@ if len(sys.argv) > 1:
 else:
     length = 16
 
-choose_from = string.letters + string.digits + '__' + '-|@.,?/!~#$%^&*(){}[]\+=.'
+#choose_from = string.letters + string.digits + '__' + '-|@.,?/!~#$%^&*(){}[]\+=.'
+choose_from = string.letters + string.digits
 
 print ''.join([random.choice(choose_from) for xx in range(length)])
