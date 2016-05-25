07a5a0408e4a3dec6acd493f160147b28ca4d76c
Scott Torborg <storborg@gmail.com>
2016-05-24 22:30:51 -0700

    Get rid of 'see' module in pythonrc, I don't really use that

--- dot.pythonrc.py
+++ dot.pythonrc.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 
-# Grab the 'see' module for useful shell introspection.
-# http://github.com/inky/see/tree/master
-try:
-    from see import see
-except ImportError:
-    pass
-
-import sys, os, atexit
+import os, atexit
 
 # Try to import readline, and if it's available, do some useful completion
 # and history tracking.
@@ -18,12 +11,12 @@ except ImportError:
 else:
     import rlcompleter
     readline.parse_and_bind("tab: complete")
-    
+
     histfile = os.path.join(os.environ['HOME'], ".python_history")
     try:
         readline.read_history_file(histfile)
     except IOError:
         pass
-    
+
     atexit.register(readline.write_history_file, histfile)
     del os, histfile
