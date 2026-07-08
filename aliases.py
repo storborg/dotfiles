5836eb94f7b052de2a0a8411580b035dacbef1e4
Scott Torborg <storborg@gmail.com>
2026-07-08 15:21:33 -0700

    Change the way .config is managed: don't try to version control dconf settings, it's obnoxious, add specific overrides instead

--- aliases.py
+++ aliases.py
@@ -5,16 +5,32 @@ Set up aliases for dotfiles and folders.
 import os
 import sys
 
-base = os.path.dirname(sys.argv[0])
+
+def safe_symlink(dest, src):
+    print("linking %s -> %s" % (src, dest))
+    try:
+        os.symlink(dest, src)
+    except OSError:
+        print("  skipping! file already exists")
+
+
+base = os.path.abspath(os.path.dirname(sys.argv[0]))
 home = os.environ["HOME"]
 
 for destfile in os.listdir(base):
-    if destfile.startswith("dot."):
+    if destfile.startswith("dot.") and (destfile != "dot.config"):
         srcfile = ".%s" % destfile[4:]
         dest = os.path.join(base, destfile)
         src = os.path.join(home, srcfile)
-        print("linking %s -> %s" % (src, dest))
-        try:
-            os.symlink(dest, src)
-        except OSError:
-            print("  skipping! file already exists")
+        safe_symlink(dest, src)
+
+
+dotconfig_path = os.path.join(home, ".config")
+if not os.path.exists(dotconfig_path):
+    os.makedirs(dotconfig_path)
+
+base_config_path = os.path.join(base, "dot.config")
+for destfile in os.listdir(base_config_path):
+    dest = os.path.join(base_config_path, destfile)
+    src = os.path.join(dotconfig_path, destfile)
+    safe_symlink(dest, src)
